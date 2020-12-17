cd03cdea198d8a44452cd68732f74222e66496a9
812jay <yongje812@gmail.com>
2020-12-17 13:54:16 +0900

    #0.11 Delete Mutation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,4 +1,4 @@
-import { getMovies, getById, addMovie } from "../db/db";
+import { getMovies, getById, addMovie, deleteMovie } from "../db/db";
 
 const resolvers = {
   Query: {
@@ -7,6 +7,7 @@ const resolvers = {
   },
   Mutation: {
     addMovie: (_, { name, score }) => addMovie(name, score),
+    deleteMovie: (_, { id }) => deleteMovie(id),
   },
 };
 
